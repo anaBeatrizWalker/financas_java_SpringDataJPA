487e5136920b8b91d971447bc85f6cd0925b077d
Ana Beatriz <anabiawalker@gmail.com>
2022-09-23 11:13:17 -0300

    Adicionando projeto SpringBoot

---
+++ src/main/java/br/fatec/financas/FinancasApplication.java
@@ -0,0 +1,13 @@
+package br.fatec.financas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fatec/financas/FinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fatec.financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
