a92b6b3282974d1211b235139455f16f98fc1303
Ana Beatriz <anabiawalker@gmail.com>
2022-09-26 12:51:24 -0300

    Adicionando herança: MappedSuperclass

--- src/main/java/br/fatec/financas/model/Conta.java
+++ src/main/java/br/fatec/financas/model/Conta.java
@@ -1,21 +1,13 @@
 package br.fatec.financas.model;
-import java.io.Serializable;
 
 //import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 //import javax.persistence.Table;
 
 //@Table(name="tb_conta")
 @Entity
-public class Conta implements Serializable {
+public class Conta extends AbstractEntity {
 	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	
 	//@Column(name="nr_agencia", nullable = false)
 	private Integer agencia;
@@ -30,20 +22,8 @@ public class Conta implements Serializable {
 	private Float saldo;
 	
 	public Conta() { }
-
-	public Conta(Long id) { 
-		this.id = id; 
-	}
 	
 	// getters e setters
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public Integer getAgencia() {
 		return agencia;
 	}
@@ -74,35 +54,5 @@ public class Conta implements Serializable {
 
 	public void setSaldo(Float saldo) {
 		this.saldo = saldo;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	
-	// hashCode e equals
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Conta other = (Conta) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}	
 }
