30d45dc047535893552605e8884330a43fc3ddb8
Ana Beatriz <anabiawalker@gmail.com>
2022-11-02 11:42:11 -0300

    Adicionando Query com Parâmetro Nomeado

--- src/main/java/br/fatec/financas/repository/ContaRepository.java
+++ src/main/java/br/fatec/financas/repository/ContaRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.fatec.financas.model.Conta;
@@ -13,8 +14,8 @@ public interface ContaRepository extends JpaRepository<Conta, Long>{
 	@Query("select c from Conta c where c.agencia=?1")
 	List<Conta> listarPorAgencia(Integer agencia);
 
-	@Query("select c from Conta c where c.agencia=?1 and " + "c.saldo between ?2 and ?3")
-	List<Conta> listarPorAgenciaESaldo(Integer agencia, Float from, Float to);
+	@Query("select c from Conta c where c.agencia=:pAgencia" + " and c.saldo between :pInicio and :pFinal")
+	List<Conta> listarPorAgenciaESaldo(@Param("pAgencia") Integer agencia, @Param("pInicio") Float from, @Param("pFinal") Float to);
 
 	@Query("select c from Conta c join Cliente cc on " + " cc.conta = c where cc.nome like %?1%")
 	List<Conta> listarPorNomeCliente(String nome);
