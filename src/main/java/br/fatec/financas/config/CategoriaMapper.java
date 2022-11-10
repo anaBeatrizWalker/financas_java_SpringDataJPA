8fbea5eb026734d07bcf18a53d98d42af77c1b20
Ana Beatriz <anabiawalker@gmail.com>
2022-11-10 14:53:20 -0300

    Adicionando ModelMapper

--- src/main/java/br/fatec/financas/config/CategoriaMapper.java
+++ src/main/java/br/fatec/financas/config/CategoriaMapper.java
@@ -3,28 +3,25 @@ package br.fatec.financas.config;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import br.fatec.financas.dto.CategoriaDTO;
 import br.fatec.financas.model.Categoria;
-import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
-@Component 
-@NoArgsConstructor 
-public class CategoriaMapper { 
-	public Categoria toEntity(CategoriaDTO categoriaDTO) { 
-		Categoria categoria = new Categoria(); 
-		categoria.setId(categoriaDTO.getId()); 
-		categoria.setNome(categoriaDTO.getNome()); 
-		return categoria; 
+@AllArgsConstructor
+@Component
+public class CategoriaMapper {
+	private ModelMapper modelMapper;
+	
+	public Categoria toEntity(CategoriaDTO categoriaDTO) {
+		return modelMapper.map(categoriaDTO, Categoria.class);
 	}
 	
-	public CategoriaDTO toDTO(Categoria categoria) { 
-		CategoriaDTO categoriaDTO = new CategoriaDTO(); 
-		categoriaDTO.setId(categoria.getId()); 
-		categoriaDTO.setNome(categoria.getNome()); 
-		return categoriaDTO; 
-	} 
+	public CategoriaDTO toDTO(Categoria categoria) {
+		return modelMapper.map(categoria, CategoriaDTO.class);
+	}
 	
 	public List<CategoriaDTO> toDTO(List<Categoria> categorias) { 
 		return categorias.stream().map(this::toDTO).collect(Collectors.toList()); 
--- src/main/java/br/fatec/financas/controller/CategoriaController.java
+++ src/main/java/br/fatec/financas/controller/CategoriaController.java
@@ -3,6 +3,8 @@ package br.fatec.financas.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,49 +20,64 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.fatec.financas.config.CategoriaMapper;
+import br.fatec.financas.dto.CategoriaDTO;
 import br.fatec.financas.model.Categoria;
 import br.fatec.financas.service.CategoriaService;
 
 @RestController
 @RequestMapping("/categorias")
-public class CategoriaController implements ControllerInterface<Categoria> {
+public class CategoriaController implements ControllerInterface<CategoriaDTO> {
 	@Autowired
 	private CategoriaService service;
+	
+	@Autowired
+	private CategoriaMapper mapper;
 
 	@Override
 	@GetMapping
-	public ResponseEntity<List<Categoria>> getAll() {
-		return ResponseEntity.ok(service.findAll());
+	public ResponseEntity<List<CategoriaDTO>> getAll() {
+		return ResponseEntity.ok(mapper.toDTO(service.findAll()));
 	}
 	
+	/*
 	@GetMapping(value = "/page") 
 	public ResponseEntity<Page<Categoria>> getAllPaginated(Pageable pageable){
 		return ResponseEntity.ok(service.findAllPaginated(pageable));
 	}
 	
+	Page<Categoria> entities = objectEntityRepository.findAll(pageable);
+	Page<CategoriaDTO> dtoPage = entities.map(new Function<Categoria, CategoriaDTO>() {
+		@Override
+		public CategoriaDTO apply(Categoria entity) {
+			return mapper.toDTO(entity);
+		}
+	});
+	*/
+	
 	@Override
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<?> get(@PathVariable("id") Long id) {
+	public ResponseEntity<CategoriaDTO> get(@PathVariable("id") Long id) {
 		Categoria categoria = service.findById(id);
 		if (categoria != null)
-			return ResponseEntity.ok(categoria);
+			return ResponseEntity.ok(mapper.toDTO(obj));
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 
 	@Override
 	@PostMapping
-	public ResponseEntity<Categoria> post(@RequestBody Categoria categoria) {
-		service.create(categoria);
+	public ResponseEntity<CategoriaDTO> post(@Valid @RequestBody CategoriaDTO obj) {
+		Categoria categoria = service.create(mapper.toEntity(obj));
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
 				.toUri();
-		return ResponseEntity.created(location).body(categoria);
+		return ResponseEntity.created(location).body(mapper.toDTO(categoria));
 	}
 
 	@Override
 	@PutMapping
-	public ResponseEntity<?> put(@RequestBody Categoria categoria) {
-		if (service.update(categoria)) {
-			return ResponseEntity.ok(categoria);
+	public ResponseEntity<?> put(@Valid @RequestBody CategoriaDTO obj) {
+		if (service.update(mapper.toEntity(obj))) {
+			return ResponseEntity.ok(obj);
 		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
